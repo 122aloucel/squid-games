4892d346135cf625ef3638c1935417775d81fa86
122aloucel <56268828+122aloucel@users.noreply.github.com>
2022-03-11 12:49:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
       showBooks: true,
       title: '2 Gay Brothers',
       author: 'Buck Fuckem',
-      age: '4'
+      age: '4',
+      x: 0,
+      y: 0,
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type)
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
     // changeTitle(abc) {
     //   this.title = abc
