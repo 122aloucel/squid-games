dbed3da5df31cd0eb9092b7447884144f6ea03c0
122aloucel <56268828+122aloucel@users.noreply.github.com>
2022-03-14 11:58:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const app = Vue.createApp({
   data() {
     return{
       showBooks: true,
-      title: '2 Gay Brothers',
-      author: 'Buck Fuckem',
-      age: '4',
-      x: 0,
-      y: 0,
+      { title: 'name of the wind', author: 'patrick rothfuss', img:'img/goku.png' },
+      { title: 'name of the wind', author: 'patrick rothfuss', img:'img/gam.png'  },
+      { title: 'name of the wind', author: 'patrick rothfuss', img:'img/granny.png'  },
+      // title: '2 Gay Brothers',
+      // author: 'Buck Fuckem',
+      // age: '4',
+      // x: 0,
+      // y: 0,
     }
   },
   methods: {
